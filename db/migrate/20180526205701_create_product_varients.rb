c8e953198fc13cb5a26ff201f0d4b7db48ae5e39
Rana Alie <alie.ammaar@gmail.com>
2018-05-27 02:26:58 +0500

    schema is ready:)

---
+++ db/migrate/20180526205701_create_product_varients.rb
@@ -0,0 +1,10 @@
+class CreateProductVarients < ActiveRecord::Migration[5.1]
+  def change
+    create_table :product_varients do |t|
+      t.string :title , null: false
+      t.float :price , null: false, precision: 15 , scale: 2
+
+      t.timestamps
+    end
+  end
+end
